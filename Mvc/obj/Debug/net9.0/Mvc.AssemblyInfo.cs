9ebb8b468bb6d8f9a21e8d99b81d69632b93cab9
hdh1512 <hhieu151203@gmail.com>
2025-08-22 12:53:45 +0700

    CapNhatVItri_CSS_HTML

--- Mvc/obj/Debug/net9.0/Mvc.AssemblyInfo.cs
+++ Mvc/obj/Debug/net9.0/Mvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+292c743baef680ff53f892296d9d92fbed9968d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dbce19d213b40b1eae5b304de6d698854e48307")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
